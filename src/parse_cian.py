89ed4a27cd8875f253df38bbd3702355058ba958
iliasakhmetov <ilyas16022001@gmail.com>
2024-05-27 16:03:55 +0300

    updated parse_cian.py. We are able to add new directories if it doesn't exist

--- src/parse_cian.py
+++ src/parse_cian.py
@@ -5,16 +5,32 @@ import datetime
 
 import cianparser
 import pandas as pd
+from pathlib import Path
+import os
 
 moscow_parser = cianparser.CianParser(location="Москва")
+
+def ensure_directories_exist():
+    """
+    Ensure that the necessary directories exist.
+    """
+    directories = ['data', 'data/raw']
+    for directory in directories:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+            print(f"Created directory: {directory}")
+        else:
+            print(f"Directory already exists: {directory}")
 
 
 def main():
     """
     Function docstring
     """
+    ensure_directories_exist()
+
     t = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
-    n_rooms = 3
+    n_rooms = 1
     csv_path = f'data/raw/{n_rooms}_{t}.csv'
     data = moscow_parser.get_flats(
         deal_type="sale",
@@ -22,7 +38,7 @@ def main():
         with_saving_csv=False,
         additional_settings={
             "start_page": 1,
-            "end_page": 50,
+            "end_page": 2,
             "object_type": "secondary"
         })
     df = pd.DataFrame(data)
